b5726215fea3286a8403966264fc387a9ea3d4c1
tawantinsuyu <asif.shakeel@gmail.com>
2016-01-22 18:10:13 -0800

    added a gnu free software license

--- mrqw.py
+++ mrqw.py
@@ -1,4 +1,25 @@
 """
+Author: Asif Shakeel
+
+Copyright 2016 Asif Shakeel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+"""
 This is a simualtion of a quantum walk on a one-dimensional lattice of length n.
 parameters controlling the walk are thetam etc. x is the position of the particle
 p is the momenttum, and m is the qubit  memory of the lattice that records
